8c8f001d58e2912bc730d4213674f26a8d1ccf0c
Saumya Singh <saumya0309singh@gmail.com>
2023-07-23 00:24:43 +0530

    All about variables

--- Basics/Variables.js
+++ Basics/Variables.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+var firstName = "Saumya"
+var lastName = "Singh"
+var email = "saumya0309singh@gmail.com"
+var password = "abcd123"
+var state = "Uttar Pradesh"
+var country = "India"
+var courseCount = 0
+var isLoggedInFromGoogle = true
+var isLoggedInFromFacebook = false
+
+console.log(`First Name : ${firstName}
+Last Name : ${lastName}
+Email : ${email}
+Password : ${password}
+State : ${state}
+Country : ${password}
+CourseCount : ${courseCount}
+Has logged in from Google? : ${isLoggedInFromGoogle}
+Has logged in from Facebook? : ${isLoggedInFromFacebook} 
+`);
